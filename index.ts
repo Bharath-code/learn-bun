10ce29210961b2e958b80e158f794fabdc13a2c8
Bharath <kumarbharath63@gmail.com>
2023-11-04 16:22:50 +0530

    handling errors

--- index.ts
+++ index.ts
@@ -11,8 +11,18 @@ const server = Bun.serve({
             const body = figlet.textSync('About Me')
             return new Response(body)
         }
+        if(url.pathname === '/feed'){
+            throw new Error('couldn\'t fetch feed')
+        }
         return new Response("Page not found, 404!!")
-    }
+    },
+    error(error) {
+        return new Response(`<pre> ${error} \n ${error.stack}</pre>`,{
+            headers:{
+                'Content-Type' : 'text/html'
+            }
+        })
+    },
 })
 
 console.log(`Listening to the server ${server.port}`)
