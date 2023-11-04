fc979f811b4f8ddcbecaf19e0f73311a8afc4e26
Bharath <kumarbharath63@gmail.com>
2023-11-04 16:16:18 +0530

    creating server and adding routes

--- index.ts
+++ index.ts
@@ -1,8 +1,18 @@
+import figlet from 'figlet';
+const server = Bun.serve({
+    port:3000,
+    fetch(req){
+        const url = new URL(req.url)
+        if(url.pathname === '/'){
+            const body = figlet.textSync('Home')
+            return new Response(body)
+        }
+        if(url.pathname === '/about'){
+            const body = figlet.textSync('About Me')
+            return new Response(body)
+        }
+        return new Response("Page not found, 404!!")
+    }
+})
 
-const z = 10
-const read = (text: string) => {
-    return text
-}
-
-const msg = read("hello world!!!")
-console.log(msg)
+console.log(`Listening to the server ${server.port}`)
